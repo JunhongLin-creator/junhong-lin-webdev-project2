35d96a179714dca8e8fce45bd23575fa843c3d17
JunhongLin-creator <1393401354@qq.com>
2021-11-16 20:55:57 -0800

    Add reducers

--- junhong-lin-webdev-project2/src/index.js
+++ junhong-lin-webdev-project2/src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Board from './Board'
+import WelcomePage from './WelcomePage';
+import reducers from './reducers/reducers';
+import { createStore} from 'redux'
+
+const store = createStore(reducers);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+      <Link to={"/"}>Home</Link>
         <Routes>
           <Route path="/" element={<WelcomePage/>} />
           <Route path="/gameBoard/:gameType" element={<Board />} />
